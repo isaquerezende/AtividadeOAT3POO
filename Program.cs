fba28e7d275aa0999ee4404161968da2bd59031c
isaquerezende <112518076+isaquerezende@users.noreply.github.com>
2023-07-01 13:06:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace questao01 {
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Entre com o estoque Mínimo:");
+            int EstoqueMinimo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o estoque Máximo:");
+            int EstoqueMaximo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Estoque Máximo: " +EstoqueMaximo);
+            Console.WriteLine("Estoque Mínumo: " +EstoqueMinimo);
+            Console.WriteLine("Média do estoque: "+ (EstoqueMaximo + EstoqueMinimo) / 2);
+
+        }
+    }
+}
